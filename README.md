2f96ad17169dc64efbf0928da222aa78d35f8655
George Rolston <grolston@outlook.com>
2020-02-10 21:39:23 -0700

    installation instructions

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ScreenConnect
 
 Simple Service Management for ConnectWise ScreenConnect solution. The following ensures that the services are started based on a system restart.
+
+## Installation
+
+Run the following:
+
+```powershell
+iex $(iwr https://raw.githubusercontent.com/grolston/screenconnect/master/Start-ScreenConnect.ps1 -UseBasicParsing).Content
+```
--- Start-ScreenConnect.ps1
+++ Start-ScreenConnect.ps1
@@ -1,25 +1,35 @@
+function Test-IsAdmin {
+  ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+}#close Test-IsAdmin
 
 $PS1FileContents = @'
 ## The following ensures the screenconnect service is running
 $ScreenConnectServices = Get-Service ScreenConnect*
-
-foreach($ScreenConnectService in $ScreenConnectServices) {
-  if($ScreenConnectService.Status -NE "Running") {
-    try {
-      Start-Service $ScreenConnectService -ErrorAction Stop
-    }
-    catch {
-      sleep -Seconds 20
-      Start-Service $ScreenConnectService
+$count = 1
+while($count -lt 2)
+  foreach($ScreenConnectService in $ScreenConnectServices) {
+    if($ScreenConnectService.Status -NE "Running") {
+      try {
+        Start-Service $ScreenConnectService -ErrorAction Stop
+      }
+      catch {
+        sleep -Seconds 20
+        Start-Service $ScreenConnectService
+      }
     }
   }
+  $count++
 }
 '@
-$PS1File = C:\ScreenConnectStartup.ps1
-$PS1FileContents | Out-File $PS1File -NoClobber:$false -Force
 
-
-# set the account which will be run under
-$RunAs = 'NT AUTHORITY\SYSTEM'
-# create the scheduled task
-schtasks /CREATE  /TN "ScreenConnect Service Start" /TR "powershell.exe -noprofile -executionpolicy Unrestricted -File $PS1File"  /RL HIGHEST /SC onstart
+if(Test-IsAdmin){
+  echo "running installer as admin"
+  $PS1File = C:\ScreenConnectStartup.ps1
+  $PS1FileContents | Out-File $PS1File -NoClobber:$false -Force
+  echo "new $PS1File created."
+  # set the account which will be run under
+  $RunAs = 'NT AUTHORITY\SYSTEM'
+  # create the scheduled task
+  schtasks /CREATE  /TN "ScreenConnect Service Start" /TR "powershell.exe -noprofile -executionpolicy Unrestricted -File $PS1File"  /RL HIGHEST /SC onstart /s $env:COMPUTERNAME  /RU $RunAs
+  echo "service scheduled to run at start executing file $PS1File"
+}
